7f6e042d651e2c55a3fb010b91f96481385ee3e3
LegitYew0108 <legitlegit0108@gmail.com>
2025-11-02 02:19:41 +0900

    feat: add constructors

--- include/urg_node2_nl/urg_node2_nl.hpp
+++ include/urg_node2_nl/urg_node2_nl.hpp
@@ -1,12 +1,57 @@
 #pragma once
+#include <csignal>
+#include <string>
+#include <thread>
+
 #include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/laser_scan.hpp"
 
 namespace urg_node2_nl
 {
+struct UrgNode2Config
+{
+  // ---- ros2 parameters ----
+  std::string ip_address_;
+  int ip_port_;
+  std::string serial_port_;
+  int serial_baud_;
+  std::string frame_id_;
+  bool calibrate_time_;
+  bool synchronize_time_;
+  bool publish_intensity_;
+  bool publish_multiecho_;
+  int error_limit_;
+  double error_reset_period_;
+  double diagnostics_tolerance_;
+  double diagnostics_window_time_;
+  double time_offset_;
+  double angle_min_;
+  double angle_max_;
+  int skip_;
+  int cluster_;
+
+  // ---- configurations without ros2 parameter ----
+  bool use_multiecho_{false};
+};
+
 class UrgNode2NL : public rclcpp::Node
 {
 public:
   explicit UrgNode2NL(const rclcpp::NodeOptions & options);
   ~UrgNode2NL();
+
+private:
+  void declare_urgnode2_parameters();
+
+  void scan_thread_func();
+  void start_thread();
+  void close_thread();
+
+  UrgNode2Config config_;
+
+  std::thread scan_thread_;
+  std::atomic_bool close_thread_flag_;
+
+  std::shared_ptr<rclcpp::Publisher<sensor_msgs::msg::LaserScan>> scan_pub_;
 };
 }  // namespace urg_node2_nl
--- src/urg_node2_nl.cpp
+++ src/urg_node2_nl.cpp
@@ -2,8 +2,69 @@
 
 namespace urg_node2_nl
 {
-UrgNode2NL::UrgNode2NL(const rclcpp::NodeOptions & options) : Node("urg_node2_nl", options) {}
+UrgNode2NL::UrgNode2NL(const rclcpp::NodeOptions & options) : Node("urg_node2_nl", options)
+{
+  std::signal(SIGPIPE, SIG_IGN);
+
+  // declare parameters
+  declare_urgnode2_parameters();
+
+  if (config_.use_multiecho_) {
+    // TODO: implement multiecho func
+  } else {
+    scan_pub_ = this->create_publisher<sensor_msgs::msg::LaserScan>("scan", rclcpp::QoS(20));
+  }
+
+  start_thread();
+}
 UrgNode2NL::~UrgNode2NL() {}
+
+void UrgNode2NL::declare_urgnode2_parameters()
+{
+  config_.ip_address_ = declare_parameter<std::string>("ip_address", "");
+  config_.ip_port_ = declare_parameter<int>("ip_port", 10940);
+  config_.serial_port_ = declare_parameter<std::string>("serial_port", "/dev/ttyACM0");
+  config_.serial_baud_ = declare_parameter<int>("serial_baud", 115200);
+  config_.frame_id_ = declare_parameter<std::string>("frame_id", "laser");
+  config_.calibrate_time_ = declare_parameter<bool>("calibrate_time", false);
+  config_.synchronize_time_ = declare_parameter<bool>("synchronize_time", false);
+  config_.publish_intensity_ = declare_parameter<bool>("publish_intensity", false);
+  config_.publish_multiecho_ = declare_parameter<bool>("publish_multiecho", false);
+  config_.error_limit_ = declare_parameter<int>("error_limit", 4);
+  config_.error_reset_period_ = declare_parameter<double>("error_reset_period", 5.0),
+  config_.diagnostics_tolerance_ = declare_parameter<double>("diagnostics_tolerance", 0.05);
+  config_.diagnostics_window_time_ = declare_parameter<double>("diagnostics_window_time", 5.0);
+  config_.time_offset_ = declare_parameter<double>("time_offset", 0.0);
+  config_.angle_min_ = declare_parameter<double>("angle_min", -M_PI);
+  config_.angle_max_ = declare_parameter<double>("angle_max", M_PI);
+  config_.skip_ = declare_parameter<int>("skip", 0);
+  config_.cluster_ = declare_parameter<int>("cluster", 1);
+}
+
+void UrgNode2NL::scan_thread_func()
+{
+  while (close_thread_flag_.load()) {
+  }
+}
+
+void UrgNode2NL::start_thread()
+{
+  // clear flag for thread finish
+  close_thread_flag_.store(false);
+  scan_thread_ = std::thread(std::bind(&UrgNode2NL::scan_thread_func, this));
+}
+
+void UrgNode2NL::close_thread()
+{
+  // set thread finish
+  close_thread_flag_.store(true);
+
+  // join thread
+  if (scan_thread_.joinable()) {
+    scan_thread_.join();
+  }
+}
+
 }  // namespace urg_node2_nl
 
 #include "rclcpp_components/register_node_macro.hpp"
